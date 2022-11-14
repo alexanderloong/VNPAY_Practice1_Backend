0128f245cd4098073acb9715774a5173542476a9
NguyenDinhThao <dj.thaomeo@gmail.com>
2022-11-14 14:53:31 +0700

    Config and create entity

---
+++ src/main/java/com/vnpay/practice1/entities/StudentEntity.java
@@ -0,0 +1,33 @@
+package com.vnpay.practice1.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "student")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String lastName;
+    private String firstName;
+    private String gender;
+    private Date birthday;
+    private String address;
+    private String phoneNumber;
+}
